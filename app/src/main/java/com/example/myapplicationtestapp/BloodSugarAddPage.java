26d398aebbd1025652d8603bc06aeaedc62f28bc
Abundance Onyeodizielu <abunonyeodi@gmail.com>
2019-11-11 20:49:47 +0000

    I add the sugar page

--- app/src/main/java/com/example/myapplicationtestapp/BloodSugarAddPage.java
+++ app/src/main/java/com/example/myapplicationtestapp/BloodSugarAddPage.java
@@ -2,7 +2,11 @@ package com.example.myapplicationtestapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class BloodSugarAddPage extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class BloodSugarAddPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blood_sugar_add_page);
+
+        FloatingActionButton fab = findViewById(R.id.floatingActionButtonSA);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent at = new Intent(BloodSugarAddPage.this, BloodSugarActivity.class);
+                startActivity(at);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/myapplicationtestapp/MedicationAddPage.java
@@ -0,0 +1,14 @@
+package com.example.myapplicationtestapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MedicationAddPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medication_add_page2);
+    }
+}
