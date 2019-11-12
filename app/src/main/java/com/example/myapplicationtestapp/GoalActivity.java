de36248627e4ceb93c728c1c8bef23e5e9d0c1fd
Abundance Onyeodizielu <abunonyeodi@gmail.com>
2019-11-12 08:42:40 +0000

    goal page

--- app/src/main/java/com/example/myapplicationtestapp/GoalActivity.java
+++ app/src/main/java/com/example/myapplicationtestapp/GoalActivity.java
@@ -2,7 +2,11 @@ package com.example.myapplicationtestapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class GoalActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class GoalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goal);
+
+        FloatingActionButton fab = findViewById(R.id.floatingActionButtonGA);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent at = new Intent(GoalActivity.this, GoalAddPage.class);
+                startActivity(at);
+            }
+        });
     }
 }
