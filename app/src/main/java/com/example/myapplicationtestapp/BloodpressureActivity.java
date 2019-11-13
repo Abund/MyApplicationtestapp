7772c855f7757465e4acc8af0ea294b8bb31ebf3
Abundance Onyeodizielu <abunonyeodi@gmail.com>
2019-11-13 16:03:59 +0000

    updates1

--- app/src/main/java/com/example/myapplicationtestapp/BloodpressureActivity.java
+++ app/src/main/java/com/example/myapplicationtestapp/BloodpressureActivity.java
@@ -1,23 +1,40 @@
 package com.example.myapplicationtestapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.myapplicationtestapp.adapters.Bloodpressureadapter;
+import com.example.myapplicationtestapp.model.BloodPressure;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import java.util.ArrayList;
 import android.view.View;
+
+import android.app.ProgressDialog;
+import android.util.Log;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class BloodpressureActivity extends AppCompatActivity {
 
     private LineChart lineChart;
+    private RecyclerView mRecycler;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.Adapter mAdapter;
+    private ArrayList<BloodPressure> data;
+    private ArrayList<String> mData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +42,15 @@ public class BloodpressureActivity extends AppCompatActivity {
         lineChart =(LineChart) findViewById(R.id.lineChart);
         //lineChart.setOnChartGestureListener(BloodpressureActivity.this);
         //lineChart.setOnChartValueSelectedListener(BloodpressureActivity.this);
+        data = new ArrayList<BloodPressure>();
+        mData = new ArrayList<>();
+        for (int i = 0;i<30;i++){
+            mData.add("new title"+i);
+        }
+        mRecycler.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(BloodpressureActivity.this,LinearLayoutManager.HORIZONTAL,false);
+        mRecycler.setLayoutManager(mLayoutManager);
+
         lineChart.setDragEnabled(true);
         lineChart.setScaleEnabled(true);
 
@@ -57,4 +83,74 @@ public class BloodpressureActivity extends AppCompatActivity {
             }
         });
     }
+
+
+//    private class SyncData extends AsyncTask<String ,String,String>{
+//        String msg="Internet/DB_Credentials/Windows_FireWall_TurnOn Error, see android monitor in the buttom for details";
+//        ProgressDialog progress;
+//        boolean success = false;
+//
+//        @Override
+//        protected void onPreExecute(){
+//            progress = ProgressDialog.show(BloodpressureActivity.this,"Synchronising","RecyclerView loading! please wait...",true);
+//        }
+//
+//        @Override
+//        protected String doInBackground(String...strings){
+//            try {
+//                Connection con = db.getConnection();
+//                ResultSet resultSet = null;
+//
+//                if (con == null) {
+//
+//                    msg="No Data found";
+//                    success = false;
+//                }else{
+//                    String query = "SELECT * FROM Product_Details where Category_ID = 2 ";
+//                    PreparedStatement stmt = con.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+//                    resultSet = stmt.executeQuery();
+//
+//                    while (resultSet.next() ) {
+//                        try {
+//                            data.add(new BloodPressure(resultSet.getBinaryStream("Image"),resultSet.getInt("Product_ID"), resultSet.getString("Product_Name"), resultSet.getInt("Price"),resultSet.getInt("Stock"), resultSet.getString("Description"), resultSet.getInt("Discount")));
+//                        }catch(Exception ex){
+//                            ex.printStackTrace();
+//                            Log.e( "error here: ",ex.getLocalizedMessage());
+//                            Log.e( "error here: ",ex.getMessage());
+//                        }
+//
+//                    }
+//                    msg ="found";
+//                    success=true;
+//                }
+//            }catch(Exception e){
+//                e.printStackTrace();
+//                //Write write = new StringWriter();
+//                //e.printStackTrace(new PrintWriter(write));
+//                //msg=writer.toString();
+//                Log.e( "error here: ",e.getLocalizedMessage());
+//                Log.e( "error here: ",e.getMessage());
+//                success=false;
+//            }
+//            return msg;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(String msg){
+//            progress.dismiss();
+//            Toast.makeText(BloodpressureActivity.this,msg+"",Toast.LENGTH_LONG).show();
+//            if(success == true){
+//                mAdapter = new Bloodpressureadapter(data,BloodpressureActivity.this);
+//                mAdapter.notifyDataSetChanged();
+//                mRecycler.setAdapter(mAdapter);
+//            }else{
+//                try{
+//
+//                }catch (Exception e){
+//
+//                }
+//
+//            }
+//        }
+//    }
 }
