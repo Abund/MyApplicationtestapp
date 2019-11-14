506d57e0803dd5f2670ea4c4f03fa8d987bc460c
Abundance Onyeodizielu <abunonyeodi@gmail.com>
2019-11-14 05:25:18 +0000

    updates3

--- app/src/main/java/com/example/myapplicationtestapp/MainActivity.java
+++ app/src/main/java/com/example/myapplicationtestapp/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.myapplicationtestapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.TextUtils;
 import android.view.View;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button signIn;
+    Button signIn,googleSignIn;
     TextView register;
+    EditText email,password;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +30,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         signIn = (Button) findViewById(R.id.signIn);
+        googleSignIn = (Button) findViewById(R.id.googleSignInButton);
         register = (TextView) findViewById(R.id.register);
+        email = (EditText) findViewById(R.id.loginEmail);
+        password = (EditText) findViewById(R.id.loginPassword);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         signIn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent at = new Intent(MainActivity.this, homescreen.class);
-                startActivity(at);
+                String email1 = email.getText().toString().trim();
+                String password1 = password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email1)){
+                    email.setError("Email is required");
+                }
+                if(TextUtils.isEmpty(password1)){
+                    password.setError("Password is required");
+                }
+
+                if(password1.length()<6){
+                    password.setError("Password must be greater than 6 characters long");
+                }
+                //authenticathe the user
+                firebaseAuth.signInWithEmailAndPassword(email1,password1).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this,"Login successful",Toast.LENGTH_SHORT).show();
+                            Intent at = new Intent(MainActivity.this, homescreen.class);
+                            startActivity(at);
+                        }else{
+                            Toast.makeText(MainActivity.this,"Error in login",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
             }
         });
 
--- app/src/main/java/com/example/myapplicationtestapp/registrationpage.java
+++ app/src/main/java/com/example/myapplicationtestapp/registrationpage.java
@@ -1,15 +1,26 @@
 package com.example.myapplicationtestapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class registrationpage extends AppCompatActivity {
 
     Button signUp;
+    FirebaseAuth firebaseAuth;
+    EditText firstName,lastName,email,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +28,50 @@ public class registrationpage extends AppCompatActivity {
         setContentView(R.layout.activity_registrationpage);
 
         signUp = (Button) findViewById(R.id.signup);
+        firstName = (EditText) findViewById(R.id.firstName);
+        lastName = (EditText) findViewById(R.id.lastName);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser()!=null){
+            Intent at = new Intent(registrationpage.this, homescreen.class);
+            startActivity(at);
+        }
 
         signUp.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent at = new Intent(registrationpage.this, homescreen.class);
-                startActivity(at);
+
+                String email1 = email.getText().toString().trim();
+                String password1 = password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email1)){
+                    email.setError("Email is required");
+                }
+                if(TextUtils.isEmpty(password1)){
+                    password.setError("Password is required");
+                }
+
+                if(password1.length()<6){
+                    password.setError("Password must be greater than 6 characters long");
+                }
+
+                // register the user in firebase
+                firebaseAuth.createUserWithEmailAndPassword(email1,password1).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(registrationpage.this,"Registration successful",Toast.LENGTH_SHORT).show();
+                            Intent at = new Intent(registrationpage.this, homescreen.class);
+                            startActivity(at);
+                        }else{
+                            Toast.makeText(registrationpage.this,"Error in registration",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
             }
         });
     }
