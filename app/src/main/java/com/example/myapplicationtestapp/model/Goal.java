7cd1716cceef298de56407ec8869c0fbc8372bd8
Abundance Onyeodizielu <abunonyeodi@gmail.com>
2019-11-15 16:59:00 +0000

    updates7

--- app/src/main/java/com/example/myapplicationtestapp/model/Goal.java
+++ app/src/main/java/com/example/myapplicationtestapp/model/Goal.java
@@ -1,4 +1,30 @@
 package com.example.myapplicationtestapp.model;
 
 public class Goal {
+    private String date;
+    private String goal;
+
+    public Goal() {
+    }
+
+    public Goal(String date, String goal) {
+        this.date = date;
+        this.goal = goal;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getGoal() {
+        return goal;
+    }
+
+    public void setGoal(String goal) {
+        this.goal = goal;
+    }
 }
