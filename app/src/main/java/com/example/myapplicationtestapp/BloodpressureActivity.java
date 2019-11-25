6dccbf44e969fd31e17f8aeff60186ca717eeafb
Abundance Onyeodizielu <abunonyeodi@gmail.com>
2019-11-25 15:08:54 +0100

    updates31

--- app/src/main/java/com/example/myapplicationtestapp/BloodpressureActivity.java
+++ app/src/main/java/com/example/myapplicationtestapp/BloodpressureActivity.java
@@ -8,17 +8,24 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.myapplicationtestapp.adapters.Bloodpressureadapter;
 import com.example.myapplicationtestapp.model.BloodPressure;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -45,9 +52,7 @@ public class BloodpressureActivity extends Fragment {
     private Bloodpressureadapter bloodpressureadapter;
     View view;
     private DatabaseReference myRefOnline;
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
+    private TextView alternate;
 
     @Nullable
     @Override
@@ -57,6 +62,7 @@ public class BloodpressureActivity extends Fragment {
         //setContentView(R.layout.activity_bloodpressure);
         lineChart = (LineChart) view.findViewById(R.id.lineChart);
         mRecycler = (RecyclerView) view.findViewById(R.id.recyclerViewBP);
+        alternate = (TextView) view.findViewById(R.id.alternate);
         //lineChart.setOnChartGestureListener(BloodpressureActivity.this);
         //lineChart.setOnChartValueSelectedListener(BloodpressureActivity.this);
         data = new ArrayList<BloodPressure>();
@@ -69,15 +75,25 @@ public class BloodpressureActivity extends Fragment {
         myRefOnline.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getChildrenCount() == 0){
+                    return;
+                }
                 for(DataSnapshot dataSnapshot1:dataSnapshot.getChildren()){
-                    if (dataSnapshot.getChildrenCount() == 0){
-                        return;
-                    }
+
                     BloodPressure bloodPressure = dataSnapshot1.getValue(BloodPressure.class);
                     data.add(bloodPressure);
                 }
+                lineChart=setUpChart(data,lineChart);
+
+                Log.e("eeeeeeeeeeeeeeeeee",data.toString());
+                if(data.isEmpty()){
+                    alternate.setVisibility(View.VISIBLE);
+                    alternate.setText("Please enter your readings");
+                }
+                //Toast.makeText(getActivity().getBaseContext(),data.toString(),Toast.LENGTH_SHORT).show();
                 bloodpressureadapter = new Bloodpressureadapter(data,getActivity().getBaseContext());
                 mRecycler.setAdapter(bloodpressureadapter);
+
             }
 
             @Override
@@ -85,31 +101,6 @@ public class BloodpressureActivity extends Fragment {
                 Toast.makeText(getActivity().getBaseContext(),"Oppss... something went wrong",Toast.LENGTH_SHORT).show();
             }
         });
-
-        lineChart.setDragEnabled(true);
-        lineChart.setScaleEnabled(true);
-
-        ArrayList<Entry> yValues = new ArrayList<>();
-
-        yValues.add(new Entry(0, 50));
-        yValues.add(new Entry(1, 60));
-        yValues.add(new Entry(2, 70));
-        yValues.add(new Entry(3, 100));
-        yValues.add(new Entry(4, 90));
-        yValues.add(new Entry(5, 100));
-
-        for(int i =0;i<data.size();i++){
-            yValues.add(new Entry(data.get(i).getDiastolicPressure(), 120));
-        }
-
-        LineDataSet set1 = new LineDataSet(yValues, "Data set 1");
-        set1.setFillAlpha(110);
-
-        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
-        dataSets.add(set1);
-
-        LineData data = new LineData(dataSets);
-        lineChart.setData(data);
 
         FloatingActionButton fab = view.findViewById(R.id.floatingActionButton);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -120,6 +111,73 @@ public class BloodpressureActivity extends Fragment {
             }
         });
         return view;
+    }
+
+    private LineChart setUpChart(ArrayList<BloodPressure> data1, LineChart lineChart) {
+        lineChart.setDragEnabled(true);
+        lineChart.setScaleEnabled(true);
+
+        LimitLine upperLimit = new LimitLine(130f,"DANGER");
+        upperLimit.setLineWidth(4f);
+        upperLimit.enableDashedLine(10f,10f,0);
+        upperLimit.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
+        upperLimit.setTextSize(15f);
+
+        LimitLine lowerLimit = new LimitLine(112f,"Too Low");
+        upperLimit.setLineWidth(4f);
+        upperLimit.enableDashedLine(10f,10f,0);
+        upperLimit.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
+        upperLimit.setTextSize(15f);
+
+        YAxis leftAxis = lineChart.getAxisLeft();
+        leftAxis.removeAllLimitLines();
+        leftAxis.addLimitLine(upperLimit);
+        leftAxis.addLimitLine(lowerLimit);
+        //leftAxis.setAxisMaximum(100f);
+        //leftAxis.setAxisMinimum(25f);
+        leftAxis.enableGridDashedLine(10f,10f,0);
+        leftAxis.setDrawLimitLinesBehindData(true);
+        lineChart.getAxisRight().setEnabled(false);
+
+//        final String[] months = new String[]{"Feb", "Feb", "Mar", "Apr", "Mar", "Apr"};
+
+        //final String[] months = new String[data.size()];
+//        for(int i =0;i<data.size();i++){
+//            months[i]=data.get(i).getDate();
+//            //yValues.add(new Entry(i, data.get(i).getDiastolicPressure()));
+//        }
+
+//        ValueFormatter formatter = new ValueFormatter() {
+//            @Override
+//            public String getAxisLabel(float value, AxisBase axis) {
+//                return months[(int) value];
+//            }
+//        };
+//        XAxis xAxis = lineChart.getXAxis();
+//        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+//        xAxis.setGranularity(1f);
+//        xAxis.setValueFormatter(formatter);
+
+        ArrayList<Entry> yValues = new ArrayList<>();
+
+        float f= (float)data1.get(0).getSystolicPressure();
+        Log.e("eeeeeeeeeeeeeeeeee",""+f);
+
+        for(int i =0;i<data1.size();i++){
+            float f1 = (float)data1.get(i).getSystolicPressure();
+            yValues.add(new Entry(i, f1));
+            Log.e("eeeeeeeeeeeeeeeeee",""+data1.get(i).getSystolicPressure());
+        }
+
+        LineDataSet set1 = new LineDataSet(yValues, "Data set 1");
+        set1.setFillAlpha(110);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+
+        LineData data = new LineData(dataSets);
+        lineChart.setData(data);
+        return lineChart;
     }
 
     @Override
